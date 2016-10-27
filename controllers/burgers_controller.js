156d34c7f1faf7f62bfb663475974c53bf9f068a
Tatiana Grigorieva <tatianagrigorieva@Tatianas-MacBook-Pro.local>
2016-10-27 07:19:31 -0400

    db routes setup

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,37 @@
+
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var app = express();
+var methodOverride = require('method-override');
+var burger = require('../models/burger.js');
+
+router.get('/', function(req,res) {
+     res.redirect('/burgers');
+});
+
+router.get('/burgers', function(req,res) {
+	burger.all(function(data){
+		var hbsObject = { burgers: data };
+		console.log(hbsObject);
+		res.render('index', hbsObject);
+	});
+});
+
+router.post('/burgers/create', function(req,res) {
+	burger.create(['burger_name', 'devoured'], [req.body.burger_name, 0], function(data){
+		res.redirect('/burgers');
+	});
+});
+
+router.put('/burgers/update/:id', function(req,res) {
+	var condition = 'id = ' + req.params.id;
+
+	console.log('condition', condition);
+
+	burger.update({ devoured: req.body.devoured }, condition, function(data){
+		res.redirect('/burgers');
+	});
+});
+
+module.exports = router;
